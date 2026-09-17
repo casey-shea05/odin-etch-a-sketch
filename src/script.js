ab0e2f482a705417491f15e2083743a22b018411
Casey <108437478+casey-shea05@users.noreply.github.com>
2026-09-17 11:38:11 +0100

    feat: add drawing functionality

--- src/script.js
+++ src/script.js
@@ -4,4 +4,28 @@ for (let i = 0; i < 256; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
   container.append(pixel);
-};
+}
+
+// Create colour function
+function colourPixel(element) {
+  if (element.classList.contains("pixel")) {
+    element.style.backgroundColor = "black";
+  }
+}
+
+// Track whether user is drawing or now
+let isDrawing = false;
+
+container.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+  colourPixel(event.target);
+});
+
+container.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    colourPixel(event.target);
+  }
+});
+
+// Use window here to track if they release mouse or go outside container
+window.addEventListener("mouseup", () => (isDrawing = false));
